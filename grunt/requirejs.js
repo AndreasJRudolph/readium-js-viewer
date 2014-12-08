d92c453af5667868cb4eaf934cf8437c30360cb2
Lars Voigt <lars.voigt@dzb.de>
2014-12-08 13:06:30 +0100

    - added IE compatibility - update grunt server file - enable requirejs optimize

--- grunt/requirejs.js
+++ grunt/requirejs.js
@@ -59,7 +59,7 @@ module.exports = function(grunt) {
                 mainConfigFile: './require_config.js',
                 include: ['ReadiumViewer'],
                 name: 'thirdparty/almond',
-                optimize: 'none',
+                //optimize: 'none',
                 baseUrl: './lib/',
                 out: 'build/cloud-reader/scripts/readium-all.js',
                 paths: {
